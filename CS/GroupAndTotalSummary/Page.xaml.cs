fc67328433d553c2fc57b688e3e7b191db54221b
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/GroupAndTotalSummary/Page.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace GroupAndTotalSummary {
+    public partial class Page : UserControl {
+        public Page() {
+            InitializeComponent();
+            AgDataGrid1.DataSource = GetData();
+        }
+
+        private List<MyDataObject> GetData() {
+            List<MyDataObject> list = new List<MyDataObject>();
+            list.Add(new MyDataObject() {
+                Item = "SD970 IS",
+                Price = 379.99m,
+                Discontinued = false,
+                Announced = new DateTime(2009, 2, 18)
+            });
+            list.Add(new MyDataObject() {
+                Item = "G10",
+                Price = 419.99m,
+                Discontinued = false,
+                Announced = new DateTime(2008, 9, 17)
+            });
+            list.Add(new MyDataObject() {
+                Item = "A580",
+                Price = 118.00m,
+                Discontinued = false,
+                Announced = new DateTime(2008, 1, 24)
+            });
+            list.Add(new MyDataObject() {
+                Item = "EOS D30",
+                Price = 275.00m,
+                Discontinued = true,
+                Announced = new DateTime(2000, 5, 17)
+            });
+            return list;
+        }
+    }
+
+    public class MyDataObject {
+        public string Item { get; set; }
+        public decimal Price { get; set; }
+        public DateTime Announced { get; set; }
+        public bool Discontinued { get; set; }
+    }
+}
---
+++ VB/GroupAndTotalSummary/Page.xaml.vb
@@ -0,0 +1,56 @@
+﻿Partial Public Class Page
+    Inherits UserControl
+
+    Public Sub New()
+        InitializeComponent()
+        AgDataGrid1.DataSource = GetData()
+		End Sub
+
+		Private Function GetData() As List(Of MyDataObject)
+			Dim list As New List(Of MyDataObject)
+			list.Add(New MyDataObject() With {.Item = "SD970 IS", .Price = 379.99D, .Discontinued = False, .Announced = New DateTime(2009, 2, 18)})
+			list.Add(New MyDataObject() With {.Item = "G10", .Price = 419.99D, .Discontinued = False, .Announced = New DateTime(2008, 9, 17)})
+			list.Add(New MyDataObject() With {.Item = "A580", .Price = 118.00D, .Discontinued = False, .Announced = New DateTime(2008, 1, 24)})
+			list.Add(New MyDataObject() With {.Item = "EOS D30", .Price = 275.00D, .Discontinued = True, .Announced = New DateTime(2000, 5, 17)})
+			Return list
+		End Function
+	End Class
+
+	Public Class MyDataObject
+		Private privateItem As String
+		Public Property Item() As String
+			Get
+				Return privateItem
+			End Get
+			Set(ByVal value As String)
+				privateItem = value
+			End Set
+		End Property
+		Private privatePrice As Decimal
+		Public Property Price() As Decimal
+			Get
+				Return privatePrice
+			End Get
+			Set(ByVal value As Decimal)
+				privatePrice = value
+			End Set
+		End Property
+		Private privateAnnounced As DateTime
+		Public Property Announced() As DateTime
+			Get
+				Return privateAnnounced
+			End Get
+			Set(ByVal value As DateTime)
+				privateAnnounced = value
+			End Set
+		End Property
+		Private privateDiscontinued As Boolean
+		Public Property Discontinued() As Boolean
+			Get
+				Return privateDiscontinued
+			End Get
+			Set(ByVal value As Boolean)
+				privateDiscontinued = value
+			End Set
+		End Property
+	End Class
